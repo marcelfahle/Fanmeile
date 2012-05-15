e4f24491219e0b1ea8dc5df32049159642b86041
Marcel Fahle <m.fahle@gmail.com>
2012-05-14 21:22:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanmeile::Application.config.secret_token = '9255f42824de2ee3aa542b6a103d155b9f0e59a19e437a0a76dcacfe28d2e96a2d0085b1a72abbf68ff1f4918a859bc2270f57475be5dfbb4249f698344f6b82'
