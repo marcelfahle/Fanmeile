b893efe6d13827f3a57f952bba5c85902309de78
Marcel Fahle <m.fahle@gmail.com>
2012-05-16 21:03:05 -0400

    creates proper admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Fanmeile::Application.routes.draw do
+
   resources :pages
 
+  namespace :admin do 
+    resources :pages
+  end
+
+  root :to => 'welcome#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +56,6 @@ Fanmeile::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
