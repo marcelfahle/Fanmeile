1936e13e21d88c999364300c10ef47ff6e4a7d5e
Marcel Fahle <m.fahle@gmail.com>
2012-06-01 07:11:39 -0400

    postfix test

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,13 +1,6 @@
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address              => "fanmeile-berlin.com",
-  :port                 => 587,
-  :domain               => "fanmeile-berlin.com",
-  :user_name            => "info@fanmeile-berlin.com",
-  :password             => "totalgeheim",
-  :authentication       => :plain,
-  :enable_starttls_auto => true
-}
-
-ActionMailer::Base.default_url_options = {
-  :host => "fanmeile-berlin.com"
+  :address => 'localhost',
+  :domain => 'www.mywebsite.com',
+  :port => 25
 }
