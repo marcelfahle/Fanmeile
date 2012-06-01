45028a20e8c5439306bc2630539b0d82024dc385
Marcel Fahle <m.fahle@gmail.com>
2012-06-01 07:20:26 -0400

    postfix test

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,5 +2,6 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => 'localhost',
   :domain => '176.58.106.139',
-  :port => 25
+  :port => 25,
+  :openssl_verify_mode  => 'none'
 }
