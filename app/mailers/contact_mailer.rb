fc9762cfd568e9d8cd416580d1fececc6f2ec68b
Marcel Fahle <m.fahle@gmail.com>
2012-06-01 09:52:10 -0400

    multiple bcc

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default from: "info@fanmeile.de"
   default :to => "m.fahle@gmail.com"
-  
+  default :bcc => "sascha.panter@gedankenwerk.com"
+
   def new_message(message)
     @message = message
-    mail(:subject => "[Fanmeile Berlin] Presse", :bcc => "sascha.panter@gedankenwerk.com")
+    mail(:subject => "[Fanmeile Berlin] Presse")
   end
 end
