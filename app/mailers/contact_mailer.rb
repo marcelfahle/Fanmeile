b812980b7d52137ae8b98db066cf93b6bfc36039
Marcel Fahle <m.fahle@gmail.com>
2012-06-01 07:48:32 -0400

    press form texting and optimizatoions

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::Base
-  default from: "marcel.fahle@gedankenwerk.com"
+  default from: "info@fanmeile.de"
   default :to => "m.fahle@gmail.com"
-  default :bcc => "sascha.panter@gedankenwerk.com, marcel.fahle@gedankenwerk.com"
+  default :bcc => "sascha.panter@gedankenwerk.com, marcel.fahle@gedankenwerk.com, michael.wirtz@me.com"
 
   def new_message(message)
     @message = message
-    mail(:subject => "[Fanmeile Berlin] ")
+    mail(:subject => "[Fanmeile Berlin] Presse")
   end
 end
